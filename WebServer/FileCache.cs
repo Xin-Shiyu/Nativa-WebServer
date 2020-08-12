5bf6590e69f86b99e065b733fae4903b8ec0dbcf
Xin-Shiyu <xinshiyu@188.com>
2020-08-12 15:55:30 +0800

    Now file access will be suspended during clearing

--- WebServer/FileCache.cs
+++ WebServer/FileCache.cs
@@ -12,14 +12,12 @@ namespace WebServer
 {
     class FileCache
     {
-        //简单的缓存机制
-        //一个不是很好的寿命机制
-
         private readonly ConcurrentDictionary<string, byte[]> cache = new ConcurrentDictionary<string, byte[]>();
         private readonly ConcurrentDictionary<string, int> lifeDict = new ConcurrentDictionary<string, int>();
         private readonly Timer timer;
         private readonly Logger logger;
         private readonly FileCacheSettings settings;
+        private bool isClearing = false;
 
         private void ExtendLife(string filename)
         {
@@ -31,6 +29,7 @@ namespace WebServer
 
         public byte[] ReadFile(string filename)
         {
+            while (isClearing) { } //清理优先，新缓存让路
             ExtendLife(filename);
             if (!cache.ContainsKey(filename)) cache.TryAdd(filename, File.ReadAllBytes(filename));
             return cache[filename];
@@ -38,6 +37,7 @@ namespace WebServer
 
         public byte[] ReadTextFile(string filename) //暂且重新编码为 UTF-8，权宜之计，以后再改。
         {
+            while (isClearing) { }
             ExtendLife(filename);
             if (!cache.ContainsKey(filename)) cache.TryAdd(filename, Encoding.UTF8.GetBytes(File.ReadAllText(filename)));
             return cache[filename];
@@ -66,6 +66,7 @@ namespace WebServer
                     --lifeDict[pair.Key];
                 }
             }
+            isClearing = true;
             killList.ForEach(
                 file =>
                 {
@@ -74,6 +75,7 @@ namespace WebServer
                     logger.Log(string.Format("清理文件缓存 {0}", file));
                 });
             GC.Collect();
+            isClearing = false;
         }
     }
 }
